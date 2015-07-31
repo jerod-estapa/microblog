04515e98bc58c7cead1c55b2c39ae6e0cbf98b5c
jerodestapa <jerodestapa@gmail.com>
2015-07-31 00:06:32 -0500

    Created base and index templates

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Jerod'}
-    return '''
-<html>
-    <head>
-        <title>Home Page</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['nickname'] + '''</h1>
-    </body>
-</html>
-'''
+    posts = [
+        {
+            'author': {'nickname': 'Lucy'},
+            'body': 'Beautiful day in Fort Collins!'
+        },
+        {
+            'author': {'nickname': 'Sadie'},
+            'body': "Can't wait for winter!"
+        }
+    ]
+    return  render_template('index.html',
+                            title="Jerod's Microblog",
+                            user=user,
+                            posts = posts)
