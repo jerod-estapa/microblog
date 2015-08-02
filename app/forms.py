8d0daf151a372f85ea32d9d61310b74c8f9b4eed
jerodestapa <jerodestapa@gmail.com>
2015-08-02 18:34:55 -0500

    Corrected spacing in forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,5 @@ from wtforms.validators import DataRequired
 
 class LoginForm(Form):
 
-
     openid = Stringfield('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
