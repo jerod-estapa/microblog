d1272723df911583d45cacdd2670dd33635e9fec
jerodestapa <jerodestapa@gmail.com>
2015-08-03 23:37:26 -0500

    Changed Stringfield to Textfield in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import Stringfield, BooleanField
+from wtforms import Textfield, BooleanField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
 
-    openid = Stringfield('openid', validators=[DataRequired()])
+    openid = Textfield('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
