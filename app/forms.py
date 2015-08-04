0782c7575ac22c1894587c20144158a0adcc1e51
jerodestapa <jerodestapa@gmail.com>
2015-08-03 23:21:10 -0500

    Added login.html to templates

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask.ext.wtf import Form
 from wtforms import Stringfield, BooleanField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(Form):
 
     openid = Stringfield('openid', validators=[DataRequired()])
